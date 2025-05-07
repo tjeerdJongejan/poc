127cc01cc9a125f5d02c9ef07890c885127317b3
tjeerdJongejan <tjeerd.jongejan@soprasteria.com>
2025-05-07 09:15:00 +0200

    Add package desription

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://github.com/tjeerdJongejan/poc/releases/download/POC/Shared.xcframework.zip",
+         checksum:"4cb8bba789d8055945774c91ff37b5257d82abb68f7f1b76a3b5a5892fc6454a")
+   ]
+)
