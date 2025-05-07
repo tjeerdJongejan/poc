1612b80be1c632ccb6ca0403d993e8d553b7e3d8
tjeerdJongejan <tjeerd.jongejan@soprasteria.com>
2025-05-07 09:51:41 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Shared",
-         url: "https://github.com/tjeerdJongejan/poc/releases/download/POC/Shared.xcframework.zip",
+         url: "curl https://github.com/tjeerdJongejan/poc/releases/download/Unspecified/Shared.xcframework.zip",
          checksum:"4cb8bba789d8055945774c91ff37b5257d82abb68f7f1b76a3b5a5892fc6454a")
    ]
 )
